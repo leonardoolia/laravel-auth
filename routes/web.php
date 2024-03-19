02dcdefa2aef29b3e70f1143653b29f6e93fc1a0
leonardoolia <leonardooliaa@gmail.com>
2024-03-19 16:56:40 +0100

    created protected routes for admin

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Admin\HomeController as AdminHomeController;
 use App\Http\Controllers\Guest\HomeController as GuestHomeController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Admin\ProjectController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,6 +20,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', GuestHomeController::class)->name('guest.home');
 
 Route::get('/admin', AdminHomeController::class)->middleware(['auth', 'verified'])->name('admin.home');
+
+Route::get('/admin/projects', [ProjectController::class, 'index'])->name('admin.projects.index')->middleware('auth');
+Route::get('/admin/projects/create', [ProjectController::class, 'create'])->name('admin.projects.create')->middleware('auth');
+Route::get('/admin/projects/{project}', [ProjectController::class, 'show'])->name('admin.projects.show')->middleware('auth');
+Route::post('/admin/projects', [ProjectController::class, 'store'])->name('admin.projects.store')->middleware('auth');
+Route::get('/admin/projects/{project}/edit', [ProjectController::class, 'edit'])->name('admin.projects.edit')->middleware('auth');
+Route::put('/admin/projects/{project}/', [ProjectController::class, 'update'])->name('admin.projects.update')->middleware('auth');
+Route::delete('/admin/projects/{project}', [ProjectController::class, 'destroy'])->name('admin.projects.destroy')->middleware('auth');
+
+
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
