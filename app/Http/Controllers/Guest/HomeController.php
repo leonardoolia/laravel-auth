e6d121698d2c29d374af5a89c9c856c24595a8e1
leonardoolia <leonardooliaa@gmail.com>
2024-03-20 15:33:59 +0100

    created home for guests

--- app/Http/Controllers/Guest/HomeController.php
+++ app/Http/Controllers/Guest/HomeController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Project;
 
 class HomeController extends Controller
 {
     public function __invoke()
     {
-        return view('guest.home');
+        $projects = Project::orderByDesc('created_at')->limit(5)->get();
+        return view('guest.home', compact('projects'));
     }
 }
