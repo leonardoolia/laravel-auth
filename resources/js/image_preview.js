68ebb658daa6c4896c2d378ea297f7e76d44e845
leonardoolia <leonardooliaa@gmail.com>
2024-03-22 14:27:24 +0100

    changed image preview script

--- resources/js/image_preview.js
+++ resources/js/image_preview.js
@@ -4,6 +4,19 @@ const placeholder = 'https://marcolanci.it/boolean/assets/placeholder.png';
 const input = document.getElementById('image');
 const preview = document.getElementById('preview');
 
+let blobUrl;
+
 input.addEventListener('input', () => {
-    preview.src = input.value ? input.value : placeholder;
+    if(Image.fil && Image.file[0]){
+        const file = image.file[0];
+        const blobUrl = URL.createObjectURL(file);
+
+        preview.src = blobUrl;
+    } else{
+        preview.src = placeholder;
+    }    
+})
+
+window.addEventListener('beforeunload', () =>{
+    if(blobUrl) URL.revokeObjectURL(blobUrl);
 })
