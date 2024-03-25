80e5125daaf2cfdf46ca4b8984d6ab6db35ab508
leonardoolia <leonardooliaa@gmail.com>
2024-03-25 16:14:54 +0100

    generated fake data for type seeder

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -2,16 +2,29 @@
 
 namespace Database\Seeders;
 
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator;
 
 class TypeSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Generator $faker): void
     {
-        //
+        // Array di labels 
+        $labels = ['Frontend', 'Backend', 'Fullstack', 'UI-UX', 'DESIGN'];
+
+        // Ciclo per girare sulle labels
+        foreach ($labels as $label) {
+            // Istanza di type
+            $type = new Type();
+
+            // Riempiamo i campi
+            $type->label = $label;
+            $type->color = $faker()->hexColor();
+        }
     }
 }
