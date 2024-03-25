f640cc6448f060c6f9cafe6d9de4a81995c84b59
leonardoolia <leonardooliaa@gmail.com>
2024-03-25 17:46:25 +0100

    create random types

--- database/factories/ProjectFactory.php
+++ database/factories/ProjectFactory.php
@@ -2,7 +2,9 @@
 
 namespace Database\Factories;
 
+use App\Models\Type;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
@@ -26,11 +28,15 @@ class ProjectFactory extends Factory
         // Salviamo le immagini sullo storage e come nome usiamo lo slug
         $img_url = Storage::putFileAs('project_images', $img, "$slug.png");
 
+        $type_ids = Type::pluck('id')->toArray();
+        $type_ids[] = null;
+
         return [
             'title' => $title,
             'slug' => Str::slug($title),
             'description' => fake()->paragraphs(15, true),
             'technologies' => fake()->text(20),
+            'type_id' => Arr::random($type_ids),
             'url' => fake()->url(),
             // 'image' => fake()->imageUrl(250, 250, true),
             // 'image' => Storage::putFile(fake()->image(storage_path('app/public/project_images'), 250, 250, true)),
