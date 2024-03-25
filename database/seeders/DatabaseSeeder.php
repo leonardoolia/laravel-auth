688dc629a6ebfa7b4120e0dfbdb032359432108d
leonardoolia <leonardooliaa@gmail.com>
2024-03-25 16:14:30 +0100

    added typeseeder in dbseeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,10 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
-        \App\Models\Project::factory(30)->create();
+        \App\Models\Project::factory(10)->create();
+
+        // Aggiungiamo il TypeSeeder
+        $this->call(TypeSeeder::class);
 
         \App\Models\User::factory()->create([
             'name' => 'Leonardo',
