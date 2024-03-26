315e80eaa19bf55f94f651a3c49f86a124fc7518
leonardoolia <leonardooliaa@gmail.com>
2024-03-26 08:41:33 +0100

    added tag link to navbar

--- resources/views/includes/layouts/navbar.blade.php
+++ resources/views/includes/layouts/navbar.blade.php
@@ -21,6 +21,9 @@
                 <li class="nav-item">
                     <a class="nav-link @if(Request::is('admin/projects*')) active @endif" href="{{route('admin.projects.index') }}">Progetti</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link @if(Request::is('admin/types*')) active @endif" href="{{route('admin.types.index') }}">Tag</a>
+                </li>
                 @endauth
             </ul>
 
